5719a1f5c79fa8202eabfe0341ac7376e111c138
xxbaizero <xxbaizero@gmail.com>
2024-01-18 17:29:30 +0800

    pro up

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,11 +1,58 @@
 public class HelloNumbers {
-    public static void main(String[] args) {
-        int x = 1;
-        int total = 0;
-        while (x <= 10) {
-            System.out.print(total + " ");
-            total = total + x;
-            x = x + 1;
+    public static void DrawTriangle(int n){
+
+        int i,j=1;
+        while (n > 0) {
+            String star = "*";
+            i = j;
+            while(i > 0) {
+                System.out.print(star);
+                i--;
+            }
+            System.out.println();
+            n--;
+            j++;
         }
-	}
+    }
+    public static void num() {
+        String star = "*";
+        int n = 1;
+        int j;
+        while (n < 6) {
+            j = n;
+            while(j > 0) {
+                System.out.print(star);
+                j--;
+            }
+            System.out.println();
+            n++;
+        }
+    }
+
+    public static void windowPosSum(int[] a, int n) {
+        /** your code here */
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > 0) {
+                for (int j = 0; j < n; j++) {
+                    if (i + j + 1 < a.length) {
+                        a[i] += a[i + j + 1];
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, -3, 4, 5, 4};
+        int n = 3;
+        windowPosSum(a, n);
+
+        // Should print 4, 8, -3, 13, 9, 4
+        System.out.println(java.util.Arrays.toString(a));
+    }
+
+
 } 
